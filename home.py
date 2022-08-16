fcc70a0083fc4844ec4eeb3ef02840e38febdc1a
sathish27sk <111362586+sathish27sk@users.noreply.github.com>
2022-08-16 18:31:15 +0530

    Create home.py

---
+++ home.py
@@ -0,0 +1,30 @@
+# task
+
+print('Hi, I can code with python programming language')
+
+print("""
+My favourite animal is cat
+
+ o-###-
+   | |
+   
+This is my home
+
+     |
+   -- --
+   |    |
+   |    |
+   |#   |______
+   |    |      |
+   |   #|    # |
+   |    |  #   |
+   ----------------
+   
+New Puzzle time
+""")
+
+born = input('What year were you born?')
+
+born = int(born)
+age = 2023 - born
+print("in the year 2023 you'll ne ", age, ' years old!')
